ad787f072acf1936e1a10dab6fc0d18ba014ab99
Atimscreative <timilehinsunday144@gmail.com>
2024-02-19 00:14:49 +0100

    Cart page ui set

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,9 @@
 import BreadCrumbs from "@/components/BreadCrumbs";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import LemonCake from "../../public/assets/lemon-cake.webp";
+import Image from "next/image";
+import { BsPlus, BsDash, BsTrash } from "react-icons/bs";
 const CartPage = () => {
   return (
     <>
@@ -24,15 +27,103 @@ const CartPage = () => {
               Cart(2)
             </h2>
 
-            <div className="hidden border-b border-main pb-2">
-              <p className="ml-8 mr-24 text-main">Product</p>
-              <div className="right flex flex-1 justify-between lg:ml-auto lg:mr-20 lg:flex-shrink-0 lg:flex-grow-0 lg:basis-1/2">
+            {/* CART FOR DESKTOP */}
+            <div className="hidden sm:block">
+              <div className="mb-5 border-b border-main pb-2 sm:grid sm:grid-cols-[2fr_1fr_1fr]">
+                <p className="text-main">Product</p>
                 <p className="w-[136px] text-main">Quantity</p>
-                <p className="w-[100px] text-center text-main">Price</p>
+                <p className="w-[100px] text-main">Sub Total</p>
+              </div>
+
+              <div className="w-full items-start justify-start sm:inline-grid sm:grid-cols-[2fr_1fr_1fr]">
+                <div className="flex gap-3">
+                  <figure className="h-[80px] w-[100px]">
+                    <Image
+                      src={LemonCake}
+                      alt="Lemon Cake"
+                      className="h-full w-full object-cover"
+                    />
+                  </figure>
+                  <h3 className="text-lg font-bold text-main">Lemon Cake</h3>
+                </div>
+                <div className="inline-flex w-[100px] items-center justify-start gap-3 rounded-[50px] bg-white px-1.5 py-1">
+                  <span className="inline-flex h-7 w-7 cursor-pointer items-center justify-center rounded-full duration-300 hover:bg-main">
+                    <BsDash size={24} />
+                  </span>
+                  <span className="font-bold">1</span>
+                  <span className="inline-flex h-7 w-7 cursor-pointer items-center justify-center rounded-full duration-300 hover:bg-main">
+                    <BsPlus size={24} />
+                  </span>
+                </div>
+                <div className="flex items-center justify-between">
+                  <span className="text-xl text-main">&euro; 300</span>
+                  <span className="cursor-pointer text-main">
+                    <BsTrash size={20} />
+                  </span>
+                </div>
               </div>
             </div>
 
-            <div></div>
+            {/* Change to Card Compoent for mobile */}
+            <div className="grid grid-cols-[170px_1fr] gap-4 sm:hidden">
+              <figure className="h-[150px] w-full">
+                <Image
+                  src={LemonCake}
+                  alt="Lemon Cake"
+                  className="h-full w-full object-cover"
+                />
+              </figure>
+              <div className="flex flex-col items-start">
+                <h3 className="text-lg font-bold text-main">Lemon Cake</h3>
+                <span className="inline-block text-lg text-main">
+                  &euro; 300
+                </span>
+                <div className="mt-3 inline-flex items-center gap-3 rounded-[50px] bg-white px-1.5 py-1">
+                  <span className="inline-flex h-7 w-7 cursor-pointer items-center justify-center rounded-full duration-300 hover:bg-main">
+                    <BsDash size={24} />
+                  </span>
+                  <span className="font-bold">1</span>
+                  <span className="inline-flex h-7 w-7 cursor-pointer items-center justify-center rounded-full duration-300 hover:bg-main">
+                    <BsPlus size={24} />
+                  </span>
+                </div>
+                <span className="mt-3 inline-flex cursor-pointer items-center gap-3 text-main">
+                  <BsTrash size={16} />
+                  Delete
+                </span>
+              </div>
+            </div>
+
+            {/* PAYMENT DETAILS */}
+            <div className="my-8 w-full border-t border-main">
+              <h2 className="mb-8 mt-4 text-xl font-bold capitalize text-main">
+                Payment Details
+              </h2>
+
+              <div className="grid sm:grid-cols-2 sm:gap-5">
+                <div>
+                  <h3 className="mb-1 font-bold text-main">Shipping Address</h3>
+                  <p className="text-main">123 Westborough Street, London</p>
+                </div>
+                <div className="flex flex-wrap justify-end">
+                  <div className="mt-6 flex w-full flex-wrap items-start justify-between sm:mt-0">
+                    <ul className="flex flex-col gap-3 text-main">
+                      <li>Product Total</li>
+                      <li>Options Total</li>
+                      <li>Grand Total</li>
+                    </ul>
+                    <ul className="flex flex-col gap-3 text-main">
+                      <li>&euro;200</li>
+                      <li>&euro;100</li>
+                      <li>&euro;300</li>
+                    </ul>
+                  </div>
+                  <button className="mt-7 self-end rounded-full bg-main px-5 py-2">
+                    Checkout
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
