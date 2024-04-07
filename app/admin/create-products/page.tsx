59291811daa5bb684dda11c096e607191ec74bb7
oma-umeh <helenumeh21@gmail.com>
2024-04-07 01:10:52 +0100

    dashboard screen in progress

--- app/admin/create-products/page.tsx
+++ app/admin/create-products/page.tsx
@@ -13,6 +13,200 @@ import ProductVariants from "@/components/admin-component/ProductVariants";
 import ProductImages from "@/components/admin-component/ProductImages";
 import StepperController from "@/components/admin-component/create-product/StepperController";
 
+const fillingsList = [
+  {
+    label: "Chocolate",
+    value: "chocolate",
+  },
+  {
+    label: "Vanilla",
+    disabled: true,
+    value: "vanilla",
+  },
+  {
+    label: "Strawberry",
+    value: "strawberry",
+  },
+  {
+    label: "Lemon",
+    value: "lemon",
+  },
+  {
+    label: "Raspberry",
+    value: "raspberry",
+  },
+  {
+    label: "Caramel",
+    value: "caramel",
+  },
+  {
+    label: "Coffee",
+    value: "coffee",
+  },
+  {
+    label: "Peanut Butter",
+    value: "peanut-butter",
+  },
+  {
+    label: "Coconut",
+    value: "coconut",
+  },
+  {
+    label: "Mint",
+    value: "mint",
+  },
+  {
+    label: "Blueberry",
+    value: "blueberry",
+  },
+  {
+    label: "Cherry",
+    value: "cherry",
+  },
+  {
+    label: "Orange",
+    value: "orange",
+  },
+  {
+    label: "Hazelnut",
+    value: "hazelnut",
+  },
+  {
+    label: "Almond",
+    value: "almond",
+  },
+];
+const cakeShapes = [
+  {
+    label: "Round",
+    value: "round",
+  },
+  {
+    label: "Rectangular",
+    value: "rectangular",
+  },
+  {
+    label: "Square",
+    value: "square",
+  },
+  {
+    label: "Heart",
+    value: "heart",
+  },
+  {
+    label: "Star",
+    value: "star",
+  },
+  {
+    label: "Oval",
+    value: "oval",
+  },
+  {
+    label: "Petal",
+    value: "petal",
+  },
+  {
+    label: "Unicorn",
+    value: "unicorn",
+  },
+  {
+    label: "Tiered",
+    value: "tiered",
+  },
+  {
+    label: "Sheet",
+    value: "sheet",
+  },
+];
+const toppings = [
+  {
+    label: "Fresh Fruit",
+    value: "fresh_fruit",
+  },
+  {
+    label: "Chocolate Ganache",
+    value: "chocolate_ganache",
+  },
+  {
+    label: "Whipped Cream",
+    value: "whipped_cream",
+  },
+  {
+    label: "Toasted Nuts",
+    value: "toasted_nuts",
+  },
+  {
+    label: "Caramel Sauce",
+    value: "caramel_sauce",
+  },
+];
+const cakeSizes = [
+  {
+    label: "Mini",
+    value: "mini",
+  },
+  {
+    label: "Small",
+    value: "small",
+  },
+  {
+    label: "Medium",
+    value: "medium",
+  },
+  {
+    label: "Large",
+    value: "large",
+  },
+  {
+    label: "Extra Large",
+    value: "extra_large",
+  },
+];
+
+const selectedSubCategory = (key: any) => {
+  switch (key.toLowerCase) {
+    case "milestone cakes":
+      return [
+        { label: "Birthday Cakes", value: "birthday_cakes" },
+        { label: "Anniversary Cakes", value: "anniversary_cakes" },
+        { label: "Graduation Cakes", value: "graduation_cakes" },
+        { label: "Baby Shower Cakes", value: "baby_shower_cakes" },
+        { label: "Retirement Cakes", value: "retirement_cakes" },
+      ];
+
+    case "kids cakes":
+      return [
+        { label: "Cartoon Character Cakes", value: "cartoon_character_cakes" },
+        { label: "Princess Cakes", value: "princess_cakes" },
+        { label: "Superhero Cakes", value: "superhero_cakes" },
+        { label: "Animal Cakes", value: "animal_cakes" },
+        { label: "Fantasy Cakes", value: "fantasy_cakes" },
+      ];
+    case "cupcakes":
+      return [
+        { label: "Classic Cupcakes", value: "classic_cupcakes" },
+        { label: "Gourmet Cupcakes", value: "gourmet_cupcakes" },
+        { label: "Vegan Cupcakes", value: "vegan_cupcakes" },
+        { label: "Gluten-Free Cupcakes", value: "gluten_free_cupcakes" },
+        { label: "Seasonal Cupcakes", value: "seasonal_cupcakes" },
+      ];
+    case "wedding cakes":
+      return [
+        {
+          label: "Traditional Wedding Cakes",
+          value: "traditional_wedding_cakes",
+        },
+        { label: "Modern Wedding Cakes", value: "modern_wedding_cakes" },
+        { label: "Floral Wedding Cakes", value: "floral_wedding_cakes" },
+        { label: "Rustic Wedding Cakes", value: "rustic_wedding_cakes" },
+        { label: "Themed Wedding Cakes", value: "themed_wedding_cakes" },
+      ];
+
+    default:
+      return;
+  }
+};
+
 const options = [
   { label: "Milestone Cakes", value: "milestone cakes", disabled: false },
   { label: "Kids' Cakes", value: "kids cakes" },
@@ -27,7 +221,7 @@ export type SelectOptionType = {
 } | null;
 
 export default function Page() {
-  const [category, setCategory] = useState<SelectOptionType>(null);
+  const [category, setCategory] = useState<any>([]);
   const [subCategory, setSubCategory] = useState([]);
 
   const [images, setImages] = useState<any>({
@@ -64,53 +258,81 @@ export default function Page() {
   return (
     <section className="p-6">
       <div className="hidden md:block">
-        <div className="flex justify-between">
+        <div className="flex items-center justify-between">
           <h1 className="font-bold">Create New Products</h1>
           <button className="rounded-md bg-black px-10 py-2 text-[12px] text-main">
             Save Changes
           </button>
         </div>
-        <hr className="my-6 text-neutral-400" />
-        <div className="grid grid-cols-2 gap-6">
-          <div>
-            <h1 className="mb-6 font-bold">Product Information</h1>
+        <hr className="my-6 border-2 text-neutral-500" />
+        <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
+          <div className="">
+            <h2 className="mb-3 font-bold">Product Information</h2>
             <div className="h-full rounded-md border border-neutral-300 p-4">
               <label htmlFor="ProductName" className="">
-                <span className="mb-1 block">Product Name</span>
+                <span className="mb-1 block after:ml-1 after:text-2xl after:text-[#E10] after:content-['*']">
+                  Product Name
+                </span>
                 <input
                   name="productName"
                   type="text"
                   autoComplete="off"
                   id="productName"
                   placeholder="Product name"
-                  className="mb-4 w-full rounded-sm border-none bg-gray-100"
+                  className="mb-4 w-full rounded-sm border-none bg-gray-100 placeholder:text-sm"
                 />
               </label>
               <label htmlFor="productDescription" className="">
-                <span className="mb-1 block">Product Description</span>
+                <span className="mb-1 block after:ml-1 after:text-2xl after:text-[#E10] after:content-['*']">
+                  Product Description
+                </span>
                 <textarea
                   name="productDescription"
                   autoComplete="off"
                   id="productDescription"
                   placeholder="Product Description"
-                  className="h-[110px] w-full resize-none rounded-sm border-none bg-gray-100"
+                  className="h-[110px] w-full resize-none rounded-sm border-none bg-gray-100 placeholder:text-sm"
                 />
               </label>
-              <div className="flex gap-4">
+              <div className="grid grid-cols-2 gap-4">
                 <div className="mt-3 w-full">
-                  <h1 className="mb-1">Product Category</h1>
+                  <h2 className="mb-1 after:ml-1 after:text-2xl after:text-[#E10] after:content-['*']">
+                    Category
+                  </h2>
 
-                  <CustomSelect
+                  {/* <CustomSelect
                     selectedOption={category}
                     setSelectOption={setCategory}
                     options={options || []}
-                  />
+                  /> */}
+
+                  <select
+                    className="form-select w-full rounded-md border-neutral-300 text-neutral-400"
+                    onChange={(e: any) => setCategory(e.target.value)}
+                    value={category}
+                  >
+                    <option
+                      className=""
+                      value={"select_category"}
+                      // disabled
+                      // selected
+                    >
+                      Select category
+                    </option>
+                    {options.map((option: any, index: number) => (
+                      <option key={index} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
                 </div>
                 <div className="mt-3 w-full">
-                  <h1 className="mb-1">Product Subcategory</h1>
+                  <h2 className="mb-1 after:ml-1 after:text-2xl after:text-[#E10] after:content-['*']">
+                    Subcategory
+                  </h2>
                   <MultiSelect
                     disabled={category == null}
-                    options={options}
+                    options={category && selectedSubCategory(category)}
                     value={subCategory}
                     onChange={setSubCategory}
                     labelledBy="Select subcategory"
@@ -119,12 +341,12 @@ export default function Page() {
               </div>
             </div>
           </div>
-          <div>
-            <h1 className="mb-6 font-bold">Product Images</h1>
-            <div className="grid h-full grid-cols-2 gap-4 rounded-md border border-neutral-300 p-4">
+          <div className="lg:mt-10 xl:mt-0">
+            <h1 className="mb-3 font-bold">Product Images</h1>
+            <div className="grid grid-cols-4 grid-rows-[180px] gap-4 rounded-md border border-neutral-300 p-4 xl:h-full xl:grid-cols-2">
               <div className="relative flex flex-col items-center justify-center overflow-hidden rounded-md border border-dashed border-neutral-300 bg-[#F8F8F8]">
                 {!images.image1 && (
-                  <div className="text-balance px-3 text-center text-neutral-400">
+                  <div className="text-balance px-3 text-center text-sm text-neutral-400">
                     Drop files here or
                     <label
                       htmlFor="image1"
@@ -170,7 +392,7 @@ export default function Page() {
               </div>
               <div className="relative flex flex-col items-center justify-center overflow-hidden rounded-md border border-dashed border-neutral-300 bg-[#F8F8F8]">
                 {!images.image2 && (
-                  <div className="text-balance px-3 text-center text-neutral-400">
+                  <div className="text-balance px-3 text-center text-sm text-neutral-400">
                     Drop files here or
                     <label
                       htmlFor="image2"
@@ -216,7 +438,7 @@ export default function Page() {
               </div>
               <div className="relative flex flex-col items-center justify-center overflow-hidden rounded-md border border-dashed border-neutral-300 bg-[#F8F8F8]">
                 {!images.image3 && (
-                  <div className="text-balance px-3 text-center text-neutral-400">
+                  <div className="text-balance px-3 text-center text-sm text-neutral-400">
                     Drop files here or
                     <label
                       htmlFor="image3"
@@ -262,7 +484,7 @@ export default function Page() {
               </div>
               <div className="relative flex flex-col items-center justify-center overflow-hidden rounded-md border border-dashed border-neutral-300 bg-[#F8F8F8]">
                 {!images.image4 && (
-                  <div className="text-balance px-3 text-center text-neutral-400">
+                  <div className="text-balance px-3 text-center text-sm text-neutral-400">
                     Drop files here or
                     <label
                       htmlFor="image4"
@@ -309,77 +531,91 @@ export default function Page() {
             </div>
           </div>
           <div className="mt-10">
-            <h1 className="mb-6 font-bold">Product Pricing</h1>
-            <div className="flex h-full gap-4 rounded-md border border-neutral-300 p-4">
-              <label htmlFor="priceFrom" className="block">
-                <span className="mb-1 inline-block">Price From</span>
-                <input
-                  name="priceFrom"
-                  type="text"
-                  autoComplete="off"
-                  id="priceFrom"
-                  placeholder="Product price ranges from"
-                  className="mb-4 w-full rounded-sm border-none bg-gray-100"
-                />
+            <h1 className="mb-3 font-bold">Product Pricing</h1>
+            <div className="flex h-full w-full gap-4 rounded-md border border-neutral-300 p-4">
+              <label htmlFor="priceFrom" className="block flex-grow">
+                <span className="mb-1 inline-block after:ml-1 after:text-2xl after:text-[#E10] after:content-['*']">
+                  Price From
+                </span>
+                <div className="relative overflow-hidden rounded-md border bg-gray-100 focus-within:border-black">
+                  <span className="absolute left-0 top-0 flex h-full w-10 items-center justify-center rounded-sm bg-white font-bold">
+                    &euro;
+                  </span>
+                  <input
+                    name="priceFrom"
+                    type="text"
+                    autoComplete="off"
+                    id="priceFrom"
+                    placeholder="Price ranges from"
+                    className="w-full border-none bg-transparent pl-12 focus:border-0 focus:ring-0"
+                  />
+                </div>
               </label>
-              <label htmlFor="priceTo" className="block">
-                <span className="mb-1 inline-block">Price To</span>
-                <input
-                  name="priceTo"
-                  type="text"
-                  autoComplete="off"
-                  id="priceTo"
-                  placeholder="Product price ranges to"
-                  className="mb-4 w-full rounded-sm border-none bg-gray-100"
-                />
+              <label htmlFor="priceTo" className="block flex-grow">
+                <span className="mb-1 inline-block after:ml-1 after:text-2xl after:text-[#E10] after:content-['*']">
+                  Price To
+                </span>
+                <div className="relative overflow-hidden rounded-md border bg-gray-100 focus-within:border-black">
+                  <span className="absolute left-0 top-0 flex h-full w-10 items-center justify-center rounded-sm bg-white font-bold">
+                    &euro;
+                  </span>
+                  <input
+                    name="priceTo"
+                    type="text"
+                    autoComplete="off"
+                    id="priceTo"
+                    placeholder="Price ranges to"
+                    className="w-full border-none bg-transparent pl-12 focus:border-0 focus:ring-0"
+                  />
+                </div>
               </label>
             </div>
           </div>
           <div className="mt-10">
-            <h1 className="mb-6 font-bold">Product Variant</h1>
-            <div className="grid h-full grid-cols-2 gap-4 rounded-md border border-neutral-300 p-4">
-              <label htmlFor="ProductName" className="block">
-                <span className="mb-1 inline-block">Product Name</span>
-                <input
-                  name="productName"
-                  type="text"
-                  autoComplete="off"
-                  id="productName"
-                  placeholder="Product name"
-                  className="mb-4 w-full rounded-sm border-none bg-gray-100"
+            <h2 className="mb-3 font-bold">Product Variant</h2>
+            <div className="grid h-full grid-cols-2 gap-2 rounded-md border border-neutral-300 p-4">
+              <label htmlFor="ProductShapes" className="block">
+                <span className="mb-1 inline-block after:ml-1 after:text-2xl after:text-[#E10] after:content-['*']">
+                  Product Shapes
+                </span>
+                <MultiSelect
+                  options={cakeShapes}
+                  value={subCategory}
+                  onChange={setSubCategory}
+                  labelledBy="Select shapes"
                 />
               </label>
-              <label htmlFor="ProductName" className="block">
-                <span className="mb-1 inline-block">Product Name</span>
-                <input
-                  name="productName"
-                  type="text"
-                  autoComplete="off"
-                  id="productName"
-                  placeholder="Product name"
-                  className="mb-4 w-full rounded-sm border-none bg-gray-100"
+              <label htmlFor="ProductSizes" className="block">
+                <span className="mb-1 inline-block after:ml-1 after:text-2xl after:text-[#E10] after:content-['*']">
+                  Product Sizes
+                </span>
+                <MultiSelect
+                  options={cakeSizes}
+                  value={subCategory}
+                  onChange={setSubCategory}
+                  labelledBy="Select sizes"
                 />
               </label>
-              <label htmlFor="ProductName" className="block">
-                <span className="mb-1 inline-block">Product Name</span>
-                <input
-                  name="productName"
-                  type="text"
-                  autoComplete="off"
-                  id="productName"
-                  placeholder="Product name"
-                  className="mb-4 w-full rounded-sm border-none bg-gray-100"
+              <label htmlFor="ProductFillings" className="block">
+                <span className="mb-1 inline-block after:ml-1 after:text-2xl after:text-[#E10] after:content-['*']">
+                  Product Fillings
+                </span>
+                <MultiSelect
+                  options={fillingsList}
+                  value={subCategory}
+                  onChange={setSubCategory}
+                  labelledBy="Select fillings"
                 />
               </label>
-              <label htmlFor="ProductName" className="block">
-                <span className="mb-1 inline-block">Product Name</span>
-                <input
-                  name="productName"
-                  type="text"
-                  autoComplete="off"
-                  id="productName"
-                  placeholder="Product name"
-                  className="mb-4 w-full rounded-sm border-none bg-gray-100"
+              <label htmlFor="ProductToppings" className="block">
+                <span className="mb-1 inline-block after:ml-1 after:text-2xl after:text-[#E10] after:content-['*']">
+                  Toppings/Add-ons
+                </span>
+                <MultiSelect
+                  options={toppings}
+                  value={subCategory}
+                  onChange={setSubCategory}
+                  labelledBy="Select toppings/add-ons"
                 />
               </label>
             </div>
--- utils/productDetails.ts
+++ utils/productDetails.ts
@@ -1,91 +1,62 @@
 export const fillingsList = [
   {
-    id: 1,
-    description: "Rich and decadent chocolate filling",
     label: "Chocolate",
     value: "chocolate",
   },
   {
-    id: 2,
-    description: "Classic and creamy vanilla filling",
     label: "Vanilla",
+    disabled: true,
     value: "vanilla",
   },
   {
-    id: 3,
-    description: "Sweet and fruity strawberry filling",
     label: "Strawberry",
     value: "strawberry",
   },
   {
-    id: 4,
-    description: "Zesty and refreshing lemon filling",
     label: "Lemon",
     value: "lemon",
   },
   {
-    id: 5,
-    description: "Tangy and flavorful raspberry filling",
     label: "Raspberry",
     value: "raspberry",
   },
   {
-    id: 6,
-    description: "Buttery and caramel-infused filling",
     label: "Caramel",
     value: "caramel",
   },
   {
-    id: 7,
-    description: "Bold and aromatic coffee-flavored filling",
     label: "Coffee",
     value: "coffee",
   },
   {
-    id: 8,
-    description: "Rich and nutty peanut butter filling",
     label: "Peanut Butter",
     value: "peanut-butter",
   },
   {
-    id: 9,
-    description: "Sweet and tropical coconut filling",
     label: "Coconut",
     value: "coconut",
   },
   {
-    id: 10,
-    description: "Refreshing and minty filling",
     label: "Mint",
     value: "mint",
   },
   {
-    id: 11,
-    description: "Juicy and tangy blueberry filling",
     label: "Blueberry",
     value: "blueberry",
   },
   {
-    id: 12,
-    description: "Sweet and tart cherry filling",
     label: "Cherry",
     value: "cherry",
   },
   {
-    id: 13,
-    description: "Citrusy and refreshing orange filling",
     label: "Orange",
     value: "orange",
   },
   {
-    id: 14,
-    description: "Nutty and aromatic hazelnut filling",
     label: "Hazelnut",
     value: "hazelnut",
   },
   {
-    id: 15,
-    description: "Nutty and flavorful almond filling",
     label: "Almond",
     value: "almond",
   },
@@ -93,67 +64,42 @@ export const fillingsList = [
 
 export const cakeShapes = [
   {
-    id: 1,
-    description: "Classic round shape, perfect for any occasion",
     label: "Round",
     value: "round",
   },
   {
-    id: 2,
-    description: "Elegant and traditional rectangular shape",
     label: "Rectangular",
     value: "rectangular",
   },
   {
-    id: 3,
-    description: "Modern and chic square shape, ideal for contemporary designs",
     label: "Square",
     value: "square",
   },
   {
-    id: 4,
-    description:
-      "Unique and whimsical heart shape, symbolizing love and romance",
     label: "Heart",
     value: "heart",
   },
   {
-    id: 5,
-    description: "Fun and playful star shape, perfect for themed parties",
     label: "Star",
     value: "star",
   },
   {
-    id: 6,
-    description:
-      "Stylish and sophisticated oval shape, adding elegance to any celebration",
     label: "Oval",
     value: "oval",
   },
   {
-    id: 7,
-    description: "Rustic and charming petal shape, evoking a natural feel",
     label: "Petal",
     value: "petal",
   },
   {
-    id: 8,
-    description:
-      "Whimsical and magical unicorn shape, perfect for children's birthdays",
     label: "Unicorn",
     value: "unicorn",
   },
   {
-    id: 9,
-    description:
-      "Creative and customizable tiered shape, adding grandeur to any event",
     label: "Tiered",
     value: "tiered",
   },
   {
-    id: 10,
-    description:
-      "Simple and versatile sheet shape, ideal for casual gatherings",
     label: "Sheet",
     value: "sheet",
   },
@@ -224,32 +170,22 @@ export const cakeTimes = [
 
 export const toppings = [
   {
-    id: 1,
-    description: "Fresh and vibrant fruit slices",
     label: "Fresh Fruit",
     value: "fresh_fruit",
   },
   {
-    id: 2,
-    description: "Smooth and velvety chocolate drizzle",
     label: "Chocolate Ganache",
     value: "chocolate_ganache",
   },
   {
-    id: 3,
-    description: "Light and airy whipped cream topping",
     label: "Whipped Cream",
     value: "whipped_cream",
   },
   {
-    id: 4,
-    description: "Crunchy and flavorful toasted nuts",
     label: "Toasted Nuts",
     value: "toasted_nuts",
   },
   {
-    id: 5,
-    description: "Sweet and buttery caramel drizzle",
     label: "Caramel Sauce",
     value: "caramel_sauce",
   },
@@ -257,32 +193,22 @@ export const toppings = [
 
 export const cakeSizes = [
   {
-    id: 1,
-    description: "Perfect for personal indulgence",
     label: "Mini",
     value: "mini",
   },
   {
-    id: 2,
-    description: "Ideal for intimate gatherings",
     label: "Small",
     value: "small",
   },
   {
-    id: 3,
-    description: "Great for small parties or celebrations",
     label: "Medium",
     value: "medium",
   },
   {
-    id: 4,
-    description: "Perfect for large gatherings or events",
     label: "Large",
     value: "large",
   },
   {
-    id: 5,
-    description: "Ideal for grand celebrations and weddings",
     label: "Extra Large",
     value: "extra_large",
   },
