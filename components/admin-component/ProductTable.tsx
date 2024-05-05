a45583f34519c8fa72c7ebd638225e99c53ee6f0
oma-umeh <helenumeh21@gmail.com>
2024-05-05 23:16:03 +0100

    pagination added to table

--- components/admin-component/ProductTable.tsx
+++ components/admin-component/ProductTable.tsx
@@ -1,15 +1,22 @@
 "use client";
-import * as React from "react";
 
 import {
   Column,
   ColumnDef,
   createColumnHelper,
+  PaginationState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+import { ArrowLeft } from "iconsax-react";
+import { useState } from "react";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import { CiSearch } from "react-icons/ci";
+import { FiChevronsLeft, FiChevronsRight } from "react-icons/fi";
 
 export default function ProductTable({
   columns,
@@ -20,10 +27,23 @@ export default function ProductTable({
   data: any;
   filteredTabs?: any;
 }) {
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 4,
+  });
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: setPagination,
+    //no need to pass pageCount or rowCount with client-side pagination as it is calculated automatically
+    state: {
+      pagination,
+    },
+    // autoResetPageIndex: false, // turn off page index reset when sorting or filtering
   });
 
   return (
@@ -111,6 +131,77 @@ export default function ProductTable({
           ))}
         </tfoot> */}
       </table>
+      <div className="mt-8 flex w-full items-center justify-between gap-2">
+        <div>
+          Showing {table.getRowModel().rows.length.toLocaleString()} of{" "}
+          {table.getRowCount().toLocaleString()} Rows
+        </div>
+        <div className="flex items-center gap-2">
+          <button
+            className="inline-flex h-10 w-10 items-center justify-center rounded-full border bg-black p-1 text-main"
+            onClick={() => table.firstPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <FiChevronsLeft />
+          </button>
+          <button
+            className="inline-flex h-10 w-10 items-center justify-center rounded-full border bg-main p-1 text-black"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <BsChevronLeft size={15} />
+          </button>
+          <button
+            className="inline-flex h-10 w-10 items-center justify-center rounded-full border bg-main p-1 text-black"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <BsChevronRight size={15} />
+          </button>
+          <button
+            className="inline-flex h-10 w-10 items-center justify-center rounded-full border bg-black p-1 text-main"
+            onClick={() => table.lastPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <FiChevronsRight />
+          </button>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <span className="flex items-center gap-1">
+            <div>Page</div>
+            <strong>
+              {table.getState().pagination.pageIndex + 1} of{" "}
+              {table.getPageCount().toLocaleString()}
+            </strong>
+          </span>
+          <span className="flex items-center gap-1">
+            | Go to page:
+            <input
+              type="number"
+              defaultValue={table.getState().pagination.pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                table.setPageIndex(page);
+              }}
+              className="w-16 rounded border p-1 text-sm focus:border-black focus:outline-none focus:ring-black"
+            />
+          </span>
+          <select
+            className="rounded border p-1 pl-4 pr-9 text-sm focus:border-black focus:outline-none focus:ring-black"
+            value={table.getState().pagination.pageSize}
+            onChange={(e) => {
+              table.setPageSize(Number(e.target.value));
+            }}
+          >
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
