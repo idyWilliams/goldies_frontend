476a3effa4bf486a5f9d1053b7e72f9f8df1a857
oma-umeh <helenumeh21@gmail.com>
2024-05-25 00:12:12 +0100

    implemented settings page with responsiveness

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,5 +1,82 @@
-import React from "react";
+"use client";
+import Accordion from "@/components/admin-component/Accordion";
+import ChangePassword from "@/components/admin-component/ChangePassword";
+import ProfileInfo from "@/components/admin-component/ProfileInfo";
+import { ArrowLeft, Lock1, Profile } from "iconsax-react";
+import React, { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
+const tabs = [
+  {
+    label: "Profile Information",
+    icon: <Profile />,
+  },
+  {
+    label: "Change Password",
+    icon: <Lock1 />,
+  },
+];
+const items = [
+  {
+    title: "Profile Information",
+    content: <ProfileInfo />,
+  },
+  {
+    title: "Change Password",
+    content: <ChangePassword />,
+  },
+];
 export default function page() {
-  return <div>settings page</div>;
+  const [selectedTab, setSelectedTab] = useState(0);
+  return (
+    <section>
+      <div className="min-h-screen px-4 py-5 lg:bg-neutral-300 lg:px-8">
+        <div className="">
+          <div className="relative mb-5 lg:mb-4 ">
+            <span className="absolute left-0 top-1/2 inline-flex -translate-y-1/2 items-center gap-2 lg:hidden">
+              {/* <ArrowLeft /> */}
+            </span>
+            <h1 className="text-center font-bold uppercase lg:mb-2 lg:text-left">
+              settings
+            </h1>
+          </div>
+          <hr className="mb-4 border-0 border-b border-neutral-500" />
+          {/* mobile */}
+          <div className="lg:hidden">
+            <h3 className="my-2">General</h3>
+            <Accordion arr={items} />
+          </div>
+          {/* desktop */}
+          <div className="hidden w-full bg-white px-8 py-5 lg:block">
+            <div className="flex gap-4">
+              {tabs.map((tab: any, index: number) => (
+                <div
+                  key={index}
+                  className={twMerge(
+                    "mb-3 inline-flex cursor-pointer items-center gap-2 bg-neutral-100 px-3 py-2 text-black duration-300",
+                    selectedTab === index && "bg-black text-main",
+                  )}
+                  onClick={() => setSelectedTab(index)}
+                >
+                  {tab.icon}
+                  <h1 className="text-[14px] capitalize">{tab.label}</h1>
+                </div>
+              ))}
+            </div>
+            <div className="w-[400px] overflow-hidden bg-white ">
+              <div
+                className={twMerge(
+                  "flex w-[800px] translate-x-0 duration-300",
+                  selectedTab === 1 && "-translate-x-1/2",
+                )}
+              >
+                <ProfileInfo />
+                <ChangePassword />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- components/admin-component/ProfileInfo.tsx
+++ components/admin-component/ProfileInfo.tsx
@@ -1,5 +1,75 @@
-import React from "react";
+"use client";
+import { Profile } from "iconsax-react";
+import React, { useState } from "react";
+import PhoneInput from "react-phone-input-2";
 
 export default function ProfileInfo() {
-  return <div>ProfileInfo</div>;
+  const [phone, setPhone] = useState("");
+  return (
+    <section className="w-full pl-2 lg:w-[400px]">
+      <>
+        <div className="mt-5 inline-flex h-10 w-10 items-center justify-center rounded-full bg-black text-main">
+          <span className="items-center justify-center">JD</span>
+        </div>
+        <p className="my-3 font-semibold">John Doe</p>
+        <form className="space-y-5">
+          <label htmlFor="fullName" className="block">
+            <span className="mb-1 inline-block">Full name</span>
+            <input
+              type="text"
+              autoComplete="off"
+              id="fullName"
+              placeholder="Your full name"
+              className="w-full rounded-sm border-none bg-gray-100"
+            />
+          </label>
+          <label htmlFor="email" className="block">
+            <span className="mb-1 inline-block">Email address</span>
+            <input
+              type="text"
+              autoComplete="off"
+              id="email"
+              placeholder="Your email"
+              className="w-full rounded-sm border-none bg-gray-100"
+            />
+          </label>
+          <label htmlFor="phoneNumber" className="block">
+            <PhoneInput
+              inputClass="w-full rounded-sm border-none bg-gray-100"
+              //   showDropdown={true}
+              enableSearch={true}
+              enableAreaCodes={true}
+              //   country={"us"}
+              value={phone}
+              onChange={setPhone}
+              inputProps={{
+                name: "phone",
+                required: true,
+                autoFocus: true,
+              }}
+              //   containerStyle={{ width: "100%" }}
+              //   inputStyle={{
+              //     width: "100%",
+              //     padding: "0.5rem",
+              //     borderRadius: "0.25rem",
+              //     border: "1px solid #ccc",
+              //   }}
+              //   buttonStyle={{
+              //     borderRadius: "0.25rem 0 0 0.25rem",
+              //     border: "1px solid #ccc",
+              //   }}
+            />
+          </label>
+        </form>
+        <div className="mb-6 mt-10 flex gap-8 lg:mb-0">
+          <button className="items-center justify-center rounded-sm border border-red-500 bg-white px-3 py-2 text-sm text-red-500 lg:text-base">
+            Cancel Changes
+          </button>
+          <button className="items-center justify-center rounded-sm bg-black px-5 py-2 text-sm text-main lg:text-base">
+            Save Changes
+          </button>
+        </div>
+      </>
+    </section>
+  );
 }
