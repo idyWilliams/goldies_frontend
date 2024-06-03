d90554c0bd36550140e6c2e34f1246b12c10b8b2
atimscreative <timilehinsunday144@gmail.com>
2024-06-03 14:41:31 +0100

    Tawk.to Chat integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import type { Metadata } from "next";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import "./globals.css";
 
 import { Provider } from "react-redux";
@@ -14,11 +14,32 @@ const metadata: Metadata = {
     "Indulge in delectable cakes from Goldies Confectioneries. Browse our wide selection of cakes for every occasion and order now for a taste of perfection!",
 };
 
+declare global {
+  interface Window {
+    Tawk_API: any;
+    Tawk_LoadStart: Date;
+  }
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
+  // TAWK_LOAD_START
+  useEffect(() => {
+    window.Tawk_API = window.Tawk_API || {};
+    window.Tawk_LoadStart = new Date();
+    (function () {
+      const s1 = document.createElement("script");
+      const s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/665b1746981b6c5647772f1f/1hv9t5rn4";
+      s1.setAttribute("crossorigin", "*");
+      s0?.parentNode?.insertBefore(s1, s0);
+    })();
+  }, []);
+
   return (
     <html lang="en">
       <head>
@@ -34,7 +55,6 @@ export default function RootLayout({
       <ShoppingCartProvider>
         <Provider store={store}>
           <ProductProvider>
-            
             <body className="overflow-x-hidden">{children}</body>
           </ProductProvider>
         </Provider>
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
+  // experimental: {
+  //   missingSuspenseWithCSRBailout: false,
+  // },
 };
 
 export default nextConfig;
