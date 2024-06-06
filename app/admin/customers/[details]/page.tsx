bf07262c304ae5c10c5dc0050169c422cfaf9eb4
oma-umeh <helenumeh21@gmail.com>
2024-06-06 23:54:29 +0100

    customer order page 95% done

--- app/admin/customers/[details]/page.tsx
+++ app/admin/customers/[details]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import CustomerOrder from "@/components/admin-component/CustomerOrder";
 import BillingInfo from "@/components/admin-component/customers/BillingInfo";
 import ContactInfo from "@/components/admin-component/customers/ContactInfo";
 import { customers } from "@/utils/adminData";
@@ -27,27 +28,30 @@ export default function Page({ params }: any) {
     }
   };
   return (
-    <section className="px-4 py-5 lg:min-h-screen lg:bg-neutral-300 lg:px-10">
-      <div className="mb-5 border-b border-neutral-500 pb-5">
-        <span
-          className="inline-flex items-center gap-2"
-          onClick={() => router.push("/admin/customers")}
-        >
-          <ArrowLeft />
-          <span className="font-bold uppercase">Customer Details</span>
-        </span>
-      </div>
-      <div className="hidden grid-cols-[35%_1fr] gap-5 lg:grid">
-        <ContactInfo />
-        <BillingInfo />
-      </div>
-      <div className="grid grid-cols-1 gap-5 lg:hidden">
-        {activeSec === "contact" && <ContactInfo />}
-        {activeSec === "billing" && <BillingInfo />}
-        <button className="text-cyan-600" onClick={handleViewMore}>
-          View More
-        </button>
-      </div>
-    </section>
+    <>
+      <section className="px-4 py-5 lg:min-h-screen lg:bg-neutral-300 lg:px-10">
+        <div className="mb-5 border-b border-neutral-500 pb-5">
+          <span
+            className="inline-flex items-center gap-2"
+            onClick={() => router.push("/admin/customers")}
+          >
+            <ArrowLeft />
+            <span className="font-bold uppercase">Customer Details</span>
+          </span>
+        </div>
+        <div className="hidden grid-cols-[35%_1fr] gap-5 lg:grid">
+          <ContactInfo />
+          <BillingInfo />
+        </div>
+        <div className="grid grid-cols-1 gap-5 lg:hidden">
+          {activeSec === "contact" && <ContactInfo />}
+          {activeSec === "billing" && <BillingInfo />}
+          <button className="text-cyan-600" onClick={handleViewMore}>
+            View More
+          </button>
+        </div>
+      </section>
+      {/* <CustomerOrder /> */}
+    </>
   );
 }
