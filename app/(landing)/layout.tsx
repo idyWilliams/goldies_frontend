be30d7d5c86861032ffdc6aee335d19b719e9cd0
atimscreative <timilehinsunday144@gmail.com>
2024-11-19 01:48:59 +0100

    User Session added

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -4,20 +4,62 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 // import { jwtDecode } from "jwt-decode";
 import AuthContext from "@/context/AuthProvider";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { jwtDecode } from "jwt-decode";
+import { userLogOut } from "@/services/hooks/user-auth";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // @ts-ignore
   const { setIsLogin, setAuth } = useContext(AuthContext);
   const router = useRouter();
+  const pathname = usePathname();
 
-  const handleLogout = () => {
-    setIsLogin(false);
-    setAuth({});
-    localStorage.removeItem("user");
-    localStorage.removeItem("accessToken");
-    localStorage.setItem("isLogin", JSON.stringify(false));
-  };
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("user") as string);
+    const userToken = storedUser ? storedUser?.token : null;
+
+    const handleSession = () => {
+      if (pathname.includes("/my-account") || pathname.includes("/my-orders")) {
+        userLogOut(router);
+      } else {
+        localStorage.setItem("isLogin", JSON.stringify(false));
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("user");
+      }
+    };
+
+    if (!userToken) {
+      console.error("User token is missing.");
+      handleSession();
+      return;
+    }
+
+    try {
+      const decodedToken: { iat: number; exp: number } = jwtDecode(userToken);
+      const storedTimestamp = decodedToken?.exp * 1000;
+      const currentTime = new Date().getTime();
+      const sessionExpired = currentTime > storedTimestamp;
+
+      console.log("sessioninfo:", {
+        storedTimestamp,
+        currentTime,
+        sessionExpired,
+      });
+
+      // When User session is still valid
+      if (!sessionExpired) {
+        console.log("Valid session admin");
+
+        return;
+      } else {
+        handleSession();
+        console.log("InValid session admin");
+      }
+    } catch (error) {
+      console.log("Error decoding token:", error, storedUser, userToken);
+      // setIsLogin(false);
+    }
+  }, [pathname, router]);
 
   return (
     <>
