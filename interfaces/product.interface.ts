1b47211dd124a6f3af2263ffafc63b556fd8a7c2
John Mason <johnmsn22@gmail.com>
2025-02-06 12:11:30 +0100

    added product interface

---
+++ interfaces/product.interface.ts
@@ -0,0 +1,30 @@
+export interface ICategory {
+  name: string;
+  id: string;
+  _id: string;
+}
+
+export interface ISubCategory {
+  name: string;
+  id: string;
+  _id: string;
+}
+
+export interface IProduct {
+  _id: string;
+  name: string;
+  description: string;
+  shapes: string[];
+  sizes: string[];
+  productType: string;
+  toppings: string[];
+  category: ICategory;
+  subCategory: ISubCategory[];
+  minPrice: string;
+  maxPrice: string;
+  images: string[];
+  flavour: string[];
+  createdAt: string;
+  updatedAt: string;
+  slug: string;
+}
