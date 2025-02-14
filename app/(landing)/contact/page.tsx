20ec4618ffa2470ed5922eb2fe149c9438b4534b
John Mason <johnmsn22@gmail.com>
2025-02-14 17:39:41 +0100

    fix: contactus form

--- app/(landing)/contact/page.tsx
+++ app/(landing)/contact/page.tsx
@@ -1,50 +1,107 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { cn } from "@/helper/cn";
+import { SendContactUsMail } from "@/interfaces/mail.interface";
+import Logo from "@/public/assets/goldis-gold-logo.png";
+import Img from "@/public/assets/reviews.png";
+import { sendContactMail } from "@/services/hooks/mail";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutation } from "@tanstack/react-query";
+import { Call, Location, Sms } from "iconsax-react";
+import { CheckCircle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
-import Img from "@/public/assets/reviews.png";
-import logo from "@/public/assets/goldis-gold-logo.png";
-import { Call, Location, Sms } from "iconsax-react";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import PhoneInput from "react-phone-input-2";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
 import "react-phone-input-2/lib/style.css";
-import { cn } from "@/helper/cn";
-import { toast } from "sonner";
-import { Button } from "@/components/ui/button";
+import * as yup from "yup";
 
 const schema = yup.object().shape({
-  name: yup.string().required("name is required"),
-  message: yup.string().required("message is required"),
+  fullName: yup.string().required("Name is required"),
+  message: yup.string().required("Message is required"),
   email: yup.string().required("Email is required").email("Email is invalid"),
-  phone: yup
+  phoneNumber: yup
     .string()
     .required("Valid Phone Number is required")
     .min(6, "Valid Phone Number must be at least 6 characters")
     .max(15, "Valid Phone Number must not exceed 12 characters"),
 });
 
-export default function Page() {
-  const [phone, setPhone] = useState("");
+export default function ContactPage() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: sendContactMail,
+    onSuccess: () => {
+      setIsDialogOpen(true);
+      reset();
+
+      setTimeout(() => {
+        setIsDialogOpen(false);
+      }, 3000);
+    },
+  });
+
   const {
     register,
     handleSubmit,
     reset,
     control,
     formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) });
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: "",
+      fullName: "",
+      message: "",
+      phoneNumber: "",
+    },
+  });
 
-  const onSubmit = (data: any) => {
-    console.log(data);
-    setPhone("");
-    reset();
-    toast.success("Form successfully submitted");
+  const onSubmit = async (data: SendContactUsMail) => {
+    await mutateAsync(data);
   };
 
   return (
-    <section className="">
+    <section>
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent className="border-black bg-black text-center">
+          <DialogHeader>
+            <DialogTitle className="text-center text-goldie-300">
+              <div className="flex justify-between ">
+                <Image
+                  src={Logo}
+                  className="flex w-[100px] items-center"
+                  alt="Goldis Logo sm"
+                />
+              </div>
+            </DialogTitle>
+          </DialogHeader>
+          <div className="flex flex-col items-center justify-center gap-4 py-4">
+            <div>
+              <CheckCircle size={50} className="text-green-500" />
+            </div>
+            <p className="text-white">
+              Your message has been sent successfully!
+            </p>
+            <Button
+              onClick={() => setIsDialogOpen(false)}
+              className="bg-goldie-300 text-[#0F0904;] hover:bg-goldie-400"
+            >
+              OK
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+
       <div className="relative h-[200px] w-full md:h-[300px]">
         <div className="relative z-20 flex h-full w-full flex-col items-center justify-center px-4">
           <h1 className="text-2xl font-bold text-white md:text-3xl lg:mb-2 lg:text-5xl">
@@ -120,7 +177,7 @@ export default function Page() {
               <div className="flex flex-col items-start space-y-7"></div>
             </div>
             <Image
-              src={logo}
+              src={Logo}
               alt="logo"
               width={100}
               height={100}
@@ -135,18 +192,18 @@ export default function Page() {
               <label htmlFor="name" className="block">
                 <span className="mb-1 inline-block font-medium">Your name</span>
                 <input
-                  {...register("name")}
+                  {...register("fullName")}
                   type="text"
                   autoComplete="off"
                   id="name"
                   placeholder="Enter your name"
                   className={cn(
                     "form-input w-full rounded-md border-gray-300 placeholder:text-sm focus:border focus:border-black focus:ring-black",
-                    errors.name && "border-red-600",
+                    errors.fullName && "border-red-600",
                   )}
                 />
-                {errors.name && (
-                  <p className="text-red-600">{errors?.name?.message}</p>
+                {errors.fullName && (
+                  <p className="text-red-600">{errors?.fullName?.message}</p>
                 )}
               </label>
               <label htmlFor="email" className="block">
@@ -173,26 +230,22 @@ export default function Page() {
                   Your phone Number
                 </span>
                 <Controller
-                  name="phone"
+                  name="phoneNumber"
                   control={control}
                   rules={{ required: true }}
-                  render={({ field: { value, onChange } }) => (
+                  render={({ field }) => (
                     <PhoneInput
                       country={"ng"}
-                      value={phone || value}
-                      onChange={(phoneNumber) => {
-                        setPhone(phoneNumber);
-                        onChange(phoneNumber);
-                      }}
+                      {...field}
                       inputProps={{
                         id: "phone",
-                        className: `form-input pl-12 w-full rounded-md placeholder:text-sm focus:border focus:border-black focus:ring-black ${errors.phone ? "border-red-600" : "border-gray-300"}`,
+                        className: `form-input pl-12 w-full rounded-md placeholder:text-sm focus:border focus:border-black focus:ring-black ${errors.phoneNumber ? "border-red-600" : "border-gray-300"}`,
                       }}
                     />
                   )}
                 />
-                {errors.phone && (
-                  <p className="text-red-600">{errors?.phone?.message}</p>
+                {errors.phoneNumber && (
+                  <p className="text-red-600">{errors?.phoneNumber?.message}</p>
                 )}
               </label>
               <label htmlFor="message" className="block">
@@ -213,8 +266,12 @@ export default function Page() {
                   <p className="text-red-600">{errors?.message?.message}</p>
                 )}
               </label>
-              <Button className="ml-auto block w-full text-goldie-300 md:w-[75%]">
-                Send Message
+              <Button
+                type="submit"
+                disabled={isPending}
+                className="ml-auto block w-full text-goldie-300 md:w-[75%]"
+              >
+                {isPending ? "Sending..." : "Send Message"}
               </Button>
             </form>
           </div>
