56b832f681881b02beb8b7b634adf9be5d8d0ba6
John Mason <johnmsn22@gmail.com>
2025-02-24 23:48:30 +0100

    fix: user order table

--- components/myOrdersComps/StatusColumn.tsx
+++ components/myOrdersComps/StatusColumn.tsx
@@ -3,27 +3,23 @@ const StatusColumn = ({ status }: { status: string }) => {
     case "completed":
       return (
         <span className="bg-green-700 px-2 py-1 text-sm text-white lg:bg-transparent lg:text-base lg:text-green-700">
-          {status}
+          Completed
         </span>
       );
     case "cancelled":
       return (
         <span className="bg-red-600 px-2 py-1 text-sm text-white lg:bg-transparent lg:text-base lg:text-red-600">
-          {status}
+          Cancelled
         </span>
       );
     case "pending":
       return (
         <span className="bg-orange-500 px-2 py-1 text-sm text-white lg:bg-transparent lg:text-base lg:text-orange-400">
-          {status}
+          Pending
         </span>
       );
     default:
-      return (
-        <span className="bg-neutral-800 px-2 py-1 text-sm text-white lg:bg-transparent lg:text-base lg:text-neutral-800">
-          {status}
-        </span>
-      );
+      return;
   }
 };
 
