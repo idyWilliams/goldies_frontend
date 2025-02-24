899bd5eb85919214506723594a8c73360dc3f5ce
John Mason <johnmsn22@gmail.com>
2025-02-24 22:34:13 +0100

    fix: order table tabs and search

--- app/(dashboard)/admin/orders/page.tsx
+++ app/(dashboard)/admin/orders/page.tsx
@@ -5,6 +5,7 @@ import MobileOrderCard from "@/components/admin-component/MobileOrderCard";
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/helper/formatCurrency";
 import { IOrder } from "@/interfaces/order.interface";
+import { cn } from "@/lib/utils";
 import { adminGetAllOrders } from "@/services/hooks/payment";
 import { useQuery } from "@tanstack/react-query";
 import { createColumnHelper } from "@tanstack/react-table";
@@ -14,6 +15,8 @@ import moment from "moment";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
 
 const statusColor = (status: string) => {
   switch (status) {
@@ -47,12 +50,21 @@ export default function OrderPage() {
   const router = useRouter();
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedStatus, setSelectedStatus] = useState("All");
+  const [searchValue, setSearchValue] = useState("");
   const itemsPerPage = 10;
 
   const { data, isLoading, refetch, isError } = useQuery({
     queryKey: ["adminAllOrders"],
     queryFn: async () => adminGetAllOrders(),
   });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  const clearInput = () => {
+    setSearchValue("");
+  };
 
   const processedOrders = useMemo<IOrder[]>(() => {
     if (!data?.orders) return [];
@@ -64,8 +76,18 @@ export default function OrderPage() {
           order.orderStatus.toLowerCase() === selectedStatus.toLowerCase(),
       );
     }
+
+    if (searchValue) {
+      filtered = filtered.filter(
+        (order) =>
+          order?.orderId?.toLowerCase().includes(searchValue.toLowerCase()) ||
+          order?.firstName?.toLowerCase().includes(searchValue.toLowerCase()) ||
+          order?.lastName?.toLowerCase().includes(searchValue.toLowerCase()),
+      );
+    }
+
     return filtered;
-  }, [data?.orders, selectedStatus]);
+  }, [data?.orders, selectedStatus, searchValue]);
 
   const totalPages = Math.ceil(processedOrders.length / itemsPerPage);
 
@@ -139,10 +161,65 @@ export default function OrderPage() {
     }),
   ];
 
+  const filteredTabs = ["All", "Pending", "Completed", "Cancelled"];
+
   return (
     <section className="h-full w-full px-4 pt-6">
       <h1 className="text-lg font-extrabold">Orders</h1>
       <hr className="my-3 mb-8 hidden border-0 border-t border-[#D4D4D4] md:block" />
+
+      <div className="my-6 flex items-center justify-between gap-4 flex-col-reverse md:flex-row">
+        {/* Filter Tabs */}
+        <div
+          className={cn(
+            "mb-6 flex items-center justify-between gap-2 p-[2px]",
+            filteredTabs?.length >= 1 && "mb-6",
+          )}
+        >
+          <div className="flex items-center gap-1">
+            {filteredTabs?.map((tab, index) => (
+              <button
+                key={index}
+                className={`w-fit rounded-sm border px-2 ${
+                  selectedStatus === tab
+                    ? "bg-black text-goldie-300"
+                    : "border-neutral-200 bg-white"
+                }`}
+                onClick={() => setSelectedStatus(tab)}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        {/* search input */}
+        <div className="w-full max-w-[500px]">
+          <label htmlFor="search" className="relative block w-full">
+            <input
+              value={searchValue}
+              type="text"
+              name="search"
+              autoComplete="search"
+              placeholder="Search..."
+              className="w-full rounded-[50px] px-4 py-2 pr-10 placeholder:text-sm focus:border-black focus:ring-black"
+              onChange={handleChange}
+            />
+            {searchValue ? (
+              <button
+                onClick={clearInput}
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
+              >
+                <IoMdClose />
+              </button>
+            ) : (
+              <span className="absolute right-3 top-1/2 -translate-y-1/2">
+                <CiSearch />
+              </span>
+            )}
+          </label>
+        </div>
+      </div>
 
       {isLoading ? (
         <div className="flex w-full items-center justify-center py-10">
@@ -163,9 +240,6 @@ export default function OrderPage() {
             <DataTable
               columns={columns}
               data={paginatedOrders}
-              filteredTabs={["All", "Pending", "Completed", "Cancelled"]}
-              statusKey="orderStatus"
-              searchKeys={["orderId", "firstName", "lastName"]}
               currentPage={currentPage}
               totalPages={totalPages}
               setCurrentPage={setCurrentPage}
--- components/admin-component/MobileOrderCard.tsx
+++ components/admin-component/MobileOrderCard.tsx
@@ -26,87 +26,56 @@ const statusColor = (status: string) => {
 };
 
 export default function MobileOrderCard({ data }: { data: IOrder[] }) {
-  const [selectedTabs, setSelectedTabs] = useState("All");
-  const [filteredData, setFilteredData] = useState(data);
-
   const router = useRouter();
-  const handleTabClick = (status: string) => {
-    setSelectedTabs(status);
-    if (status === "All") {
-      setFilteredData(data);
-    } else {
-      setFilteredData(
-        data.filter(
-          (item) => item?.orderStatus.toLowerCase() === status.toLowerCase(),
-        ),
-      );
-    }
-  };
-  return (
-    <div className="">
-      <div className="mt-3 flex items-center gap-1">
-        {["All", "Pending", "Completed", "Cancelled"].map(
-          (tabs: string, index: number) => (
-            <button
-              key={index}
-              className={`w-fit rounded-sm border px-2 ${selectedTabs === tabs ? "bg-black text-goldie-300" : "border-neutral-200 bg-white"}`}
-              onClick={() => {
-                handleTabClick(tabs);
-              }}
-            >
-              {tabs}
-            </button>
-          ),
-        )}
-      </div>
-      <div className="mt-5 grid grid-cols-1 gap-4">
-        {filteredData.map((data: IOrder, index: number) => {
-          return (
-            <Card key={index} className="bg-white p-4 shadow-xl">
-              <div className="flex flex-col gap-3">
-                <div className="flex flex-col">
-                  <div className="grid grid-cols-2">
-                    <div className="flex flex-col items-start">
-                      <span className="inline-block text-sm font-semibold">
-                        Order ID: {data.orderId}
-                      </span>
-                      <span className="mb-1.5 mt-1 inline-block text-sm font-medium">
-                        <span className="font-semibold">Total:</span>{" "}
-                        {formatCurrency(data.fee.total, "en-NG")}
-                      </span>
-                    </div>
 
-                    {/* timestamp */}
-                    <div className="flex flex-col text-right ">
-                      <span className="text-sm">
-                        {moment(data.createdAt).format("MMM DD, YYYY HH:mm A")}
-                      </span>
-                      <span className="text-sm">
-                        {statusColor(data.orderStatus)}
-                      </span>
-                    </div>
+  return (
+    <div className=" grid grid-cols-1 gap-4">
+      {data.map((data: IOrder, index: number) => {
+        return (
+          <Card key={index} className="bg-white p-4 shadow-xl">
+            <div className="flex flex-col gap-3">
+              <div className="flex flex-col">
+                <div className="grid grid-cols-2">
+                  <div className="flex flex-col items-start">
+                    <span className="inline-block text-sm font-semibold">
+                      Order ID: {data.orderId}
+                    </span>
+                    <span className="mb-1.5 mt-1 inline-block text-sm font-medium">
+                      <span className="font-semibold">Total:</span>{" "}
+                      {formatCurrency(data.fee.total, "en-NG")}
+                    </span>
                   </div>
-                  <div>
-                    <span className="mb-1.5 inline-block text-sm font-medium">
-                      <span className="font-semibold">Billing Name:</span>{" "}
-                      {data?.firstName + " " + data.lastName}
+
+                  {/* timestamp */}
+                  <div className="flex flex-col text-right ">
+                    <span className="text-sm">
+                      {moment(data.createdAt).format("MMM DD, YYYY HH:mm A")}
+                    </span>
+                    <span className="text-sm">
+                      {statusColor(data.orderStatus)}
                     </span>
                   </div>
                 </div>
-
-                <div className="flex justify-end">
-                  <Button
-                    className="rounded-sm bg-goldie-300 text-black hover:bg-goldie-400"
-                    onClick={() => router.push(`/admin/orders/${data._id}`)}
-                  >
-                    View Details
-                  </Button>
+                <div>
+                  <span className="mb-1.5 inline-block text-sm font-medium">
+                    <span className="font-semibold">Billing Name:</span>{" "}
+                    {data?.firstName + " " + data.lastName}
+                  </span>
                 </div>
               </div>
-            </Card>
-          );
-        })}
-      </div>
+
+              <div className="flex justify-end">
+                <Button
+                  className="rounded-sm bg-goldie-300 text-black hover:bg-goldie-400"
+                  onClick={() => router.push(`/admin/orders/${data._id}`)}
+                >
+                  View Details
+                </Button>
+              </div>
+            </div>
+          </Card>
+        );
+      })}
     </div>
   );
 }
