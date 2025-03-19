495d22ee39f99d1066c455af5e3fd8d81a560857
John Mason <johnmsn22@gmail.com>
2025-03-19 10:25:05 +0100

    update on cart

--- services/hooks/cart/useCart.ts
+++ services/hooks/cart/useCart.ts
@@ -2,23 +2,43 @@
 import { addToCartStoreDTO } from "@/interfaces/cart.interface";
 import {
   addToCart as addProductToCart,
-  clearCartFromStore,
+  setCart,
 } from "@/redux/features/product/cartSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hook";
+import { useQuery } from "@tanstack/react-query";
+import { getCartList } from ".";
+import { useCallback, useEffect } from "react";
 
 const useCart = () => {
   const dispatch = useAppDispatch();
   const { cart } = useAppSelector((state) => state.cart);
 
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ["cartList"],
+    queryFn: getCartList,
+  });
+
   // Function to handle adding an item to the cart
   const handleAddToCart = (item: addToCartStoreDTO) => {
     dispatch(addProductToCart(item));
+    // refetch();
   };
+
+  // Sync Redux cart with server cart on mount
+  const updateCart = useCallback(() => {
+    if (data) {
+      dispatch(setCart(data?.cart?.products));
+    }
+  }, [data, dispatch]);
+
+  useEffect(() => {
+    updateCart();
+  }, [updateCart]);
 
   return {
     cart,
     handleAddToCart,
-    isLoading: false,
+    isLoading,
   };
 };
 
