998113a6c782c62061f26dfe0a87f82791e4ce2b
Williams <widorenyin0@gmail.com>
2025-04-15 19:33:47 +0100

    update stats

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,19 +14,62 @@ import Slide2 from "../public/assets/slides/slide-2.webp";
 import Slide3 from "../public/assets/slides/slide-3.webp";
 import Slide4 from "../public/assets/slide-4.jpeg";
 import Slide5 from "../public/assets/slide-5.jpeg";
-import { useState } from "react";
+import { AwaitedReactNode, JSXElementConstructor, Key, ReactElement, ReactNode, ReactPortal, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
-
-
-const stats = [
-  { value: "12000+", title: "Cakes Delivered" },
-  { value: "12000+", title: "Verified Customers" },
-  { value: "500+", title: "Cakes Recipes" },
-];
+import { useQuery } from "@tanstack/react-query";
+import { getCustomersAnalytics } from "@/services/hooks/summary";
+import { Cake, User, Book } from "iconsax-react";
+import { FaSpinner } from "react-icons/fa";
 
 const Hero = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const router = useRouter();
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["analytics"],
+    queryFn: getCustomersAnalytics,
+  });
+
+  const stats = useMemo(() => {
+    if (isLoading || isError || !data?.data) {
+      return [
+        {
+          icon: <Cake size={24} variant="Bold" />,
+          value: "Loading...",
+          title: "Cakes Delivered",
+        },
+        {
+          icon: <User size={24} variant="Bold" />,
+          value: "Loading...",
+          title: "Verified Customers",
+        },
+        {
+          icon: <Book size={24} variant="Bold" />,
+          value: "Loading...",
+          title: "Cakes Recipes",
+        },
+      ];
+    }
+
+    const { cakesDelivered, verifiedCustomers, recipesCount } = data.data;
+
+    return [
+      {
+        icon: <Cake size={24} variant="Bold" />,
+        value: cakesDelivered > 0 ? `${cakesDelivered}+` : "0",
+        title: "Cakes Delivered",
+      },
+      {
+        icon: <User size={24} variant="Bold" />,
+        value: verifiedCustomers > 0 ? `${verifiedCustomers}+` : "0",
+        title: "Verified Customers",
+      },
+      {
+        icon: <Book size={24} variant="Bold" />,
+        value: recipesCount > 0 ? `${recipesCount}+` : "0",
+        title: "Cakes Recipes",
+      },
+    ];
+  }, [data, isLoading, isError]);
 
   return (
     <section className="relative h-auto">
@@ -40,9 +83,6 @@ const Hero = () => {
         effect={"fade"}
         fadeEffect={{ crossFade: true }}
         loop
-        // pagination={{
-        //   clickable: true,
-        // }}
         modules={[Autoplay, Pagination, Navigation, EffectFade]}
         className="mySwiper h-[70vh] w-full md:h-[50vh] lg:h-screen"
       >
@@ -51,18 +91,6 @@ const Hero = () => {
           className="relative items-center overflow-hidden"
         >
           <div className="absolute inset-0 -z-10"></div>
-
-          {/* {!isLoaded && (
-            <Image
-              src={HeroBg}
-              alt="hero cake"
-              fill
-              sizes="(max-width: 1024px) 75vw "
-              priority
-              className=" -z-50 object-cover object-center"
-              placeholder="blur"
-            />
-          )} */}
 
           <Image
             src={Slide1}
@@ -92,7 +120,6 @@ const Hero = () => {
               >
                 Order Now
               </Button>
-
             </div>
           </div>
         </SwiperSlide>
@@ -165,91 +192,33 @@ const Hero = () => {
             </div>
           </div>
         </SwiperSlide>
-        {/*   <SwiperSlide
-          style={{ display: "flex" }}
-          className="relative  items-center overflow-hidden"
-        >
-          <div className="absolute inset-0 -z-10"></div>
-          <Image
-            src={Slide4}
-            alt="hero cake"
-            sizes="(max-width: 768px) 50vw, (max-width: 1024px) 75vw, 33vw"
-            fill
-            className=" -z-50 object-cover object-center"
-            placeholder="blur"
-          />
-          <div className="wrapper">
-             <div className="md:w-[50%] xl:w-[40%]">
-              <h1 className="text-3xl font-bold text-white xl:text-6xl xl:leading-[1.3]">
-                Customized themed Cake
-              </h1>
-              <p className="text-balance text-xl text-white lg:leading-[1.5] xl:mt-4 xl:text-2xl">
-                Customized to your satisfaction.
-              </p>
-              <Button
-                size="lg"
-                className="mt-6 bg-goldie-300 font-semibold text-black hover:bg-goldie-300"
-                onClick={() => {
-                  console.log("click order now button on slide 4");
-                  router.push("/shop");
-                }}
-              >
-                Order Now
-              </Button>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide
-          style={{ display: "flex" }}
-          className="relative  items-center overflow-hidden"
-        >
-          <div className="absolute inset-0 -z-10"></div>
-          <Image
-            src={Slide5}
-            alt="hero cake"
-            sizes="(max-width: 768px) 50vw, (max-width: 1024px) 75vw, 33vw"
-            fill
-            className=" -z-50 object-cover object-center"
-            placeholder="blur"
-          />
-          <div className="wrapper">
-            <div className="lg:w-[30%] xl:w-[40%]">
-              <h1 className="text-3xl font-bold text-white xl:text-balance xl:text-6xl xl:leading-[1.3]">
-                Cupcakes Just for you
-              </h1>
-              <p className="text-balance text-xl text-white lg:leading-[1.5] xl:mt-4 xl:text-2xl">
-                Unwrap the joy with every bite.
-              </p>
-              <Button
-                size="lg"
-                className="mt-6 bg-brand-200 font-semibold text-brand-100 hover:bg-brand-200"
-                onClick={() => {
-                  console.log("click order now button on slide 5");
-                  router.push("/shop");
-                }}
-              >
-                Order Now
-              </Button>
-            </div>
-          </div>
-        </SwiperSlide> */}
       </Swiper>
       {/* STATS */}
       <div className="bottom-0 left-0 z-10 w-full bg-brand-200 py-3 lg:absolute">
         <div className="wrapper flex w-full flex-col items-center justify-between sm:justify-evenly sm:gap-8 md:flex-row md:divide-y-0">
-          <EachElement
-            of={stats}
-            render={(item: any, index: number) => (
-              <div key={index} className="w-full py-2 text-center">
-                <h3 className="text-2xl font-semibold text-brand-100 md:text-3xl">
-                  {item?.value}
-                </h3>
-                <p className="text-sm font-normal text-brand-100">
-                  {item?.title}
-                </p>
-              </div>
-            )}
-          />
+          {isLoading ? (
+            <div className="flex w-full items-center justify-center py-4">
+              <FaSpinner size={32} className="animate-spin text-brand-100" />
+            </div>
+          ) : (
+            <EachElement
+              of={stats}
+              render={(item: { icon: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined; value: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined; title: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined; }, index: Key | null | undefined) => (
+                <div
+                  key={index}
+                  className="flex w-full flex-col items-center py-2 text-center"
+                >
+                  {/* <div className="mb-2 text-brand-100">{item.icon}</div> */}
+                  <h3 className="text-2xl font-semibold text-brand-100 md:text-3xl">
+                    {item.value}
+                  </h3>
+                  <p className="text-sm font-normal text-brand-100">
+                    {item.title}
+                  </p>
+                </div>
+              )}
+            />
+          )}
         </div>
       </div>
     </section>
